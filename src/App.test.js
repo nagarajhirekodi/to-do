c5670b561f678ab379ae59a3e1058bff0dde4656
Nagaraj <nagaraj.hirekodi@infosys.com>
2021-01-15 22:50:51 +0530

    initial commits

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,12 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import * as React from "react";
+import Adapter from "enzyme-adapter-react-16";
+import { configure, shallow } from "enzyme";
+import App from "./App";
+configure({ adapter: new Adapter() });
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("Should render App component", () => {
+  const wrapper = shallow(<App />);
+  it("Should render App component", () => {
+    expect(wrapper.exists()).toBe(true);
+  });
 });
